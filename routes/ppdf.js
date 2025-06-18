669386144d3e9321d9c5ea7a75530a58cec71082
Ry923g <ryoga.o.sk@gmail.com>
2025-06-18 14:21:36 +0900

    Add files via upload

--- routes/ppdf.js
+++ routes/ppdf.js
@@ -90,9 +90,7 @@ router.get('/pdf', async (req, res) => {
   try {
     console.log('puppeteer起動前');
 const browser = await puppeteer.launch({
-   executablePath: '/opt/render/.cache/puppeteer/chrome/linux-137.0.7151.70/chrome-linux64/chrome',
-   args: ['--no-sandbox', '--disable-setuid-sandbox'] 
-  });
+  args: ['--no-sandbox', '--disable-setuid-sandbox']});
 console.log('puppeteer起動後');
 const page = await browser.newPage();
 console.log('newPage作成後');
